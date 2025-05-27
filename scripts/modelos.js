568e2ac4e99c93d0a80590354279d6358153c3d9
TorresAnndy <andygonzales2003@gmail.com>
2025-05-26 20:01:04 -0500

    modified:   scripts/app.js 	modified:   scripts/modelos.js 	modified:   style/estilos.css

--- scripts/modelos.js
+++ scripts/modelos.js
@@ -3,40 +3,44 @@ var URL_API = "https://pokeapi.co/api/v2/pokemon/";
 var URL_API_TIPO = "https://pokeapi.co/api/v2/type/";
 
 var PokemonModel = Backbone.Model.extend({
-    obtenerDatos: function(){
-        var nombre = this.get('nombre').toLowerCase();
-        $.getJSON(URL_API + nombre)
-            .done((data) => {
-                this.set({
-                    imagen: data.sprites.front_default,
-                    tipo: data.types.map(t => t.type.name).join(', '),
-                    altura: data.height,
-                    peso: data.weight,
-                    legendario: false, // se puede mejorar con otra llamada a species
-                    nombre: data.name
-                });
-                this.trigger('datos:listos');
-            })
-            .fail(()=>{
-                alert("Pokémon no encontrado");
-            });
-    }
+  obtenerDatos: function () {
+    var nombre = this.get("nombre").toLowerCase();
+    $.getJSON(URL_API + nombre)
+      .done((data) => {
+        this.set({
+          imagen: data.sprites.front_default,
+          tipo: data.types.map((t) => t.type.name).join(", "),
+          altura: data.height,
+          peso: data.weight,
+          legendario: false, // se puede mejorar con otra llamada a species
+          nombre: data.name,
+        });
+        this.trigger("datos:listos");
+      })
+      .fail(() => {
+        alert("Pokémon no encontrado");
+      });
+  },
 });
 
 var PokedexCollection = Backbone.Collection.extend({
-    model: PokemonModel,
+  model: PokemonModel,
 
-    obtenerPorTipo: function(tipo) {
-        var self = this;
-        this.reset();
-        $.getJSON(URL_API_TIPO + tipo)
-            .done(function(data) {
-                var pokemons = data.pokemon.slice(0, 10); // limitar a 10
-                pokemons.forEach(p => {
-                    var poke = new PokemonModel({ nombre: p.pokemon.name });
-                    poke.obtenerDatos();
-                    self.add(poke);
-                });
-            });
-    }
+  obtenerPorTipo: function (tipo) {
+    var self = this;
+    this.reset();
+    $.getJSON(URL_API_TIPO + tipo).done(function (data) {
+      var pokemons = data.pokemon.slice(0, 10); // limitar a 10
+      pokemons.forEach((p) => {
+        var poke = new PokemonModel({ nombre: p.pokemon.name });
+
+        // Espera a que los datos estén listos antes de agregarlo a la colección
+        poke.on("datos:listos", function () {
+          self.add(poke);
+        });
+
+        poke.obtenerDatos(); // Hace la petición a la API y llena imagen, tipo, etc.
+      });
+    });
+  },
 });
